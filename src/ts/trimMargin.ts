e010736b4d0f364396b0d4e5cfc411696b64a6f3
talog <tee.talog@gmail.com>
2018-01-24 22:57:53 +0900

    add types

--- src/ts/trimMargin.ts
+++ src/ts/trimMargin.ts
@@ -1,34 +1,34 @@
 const { defalutDelimiter } = require("./config");
 
-const trimMargin = function trimMargin (str, delimiter = defalutDelimiter) {
+const trimMargin = function trimMargin (str: string, delimiter: string = defalutDelimiter): string {
 	if (delimiter === defalutDelimiter) {
 		// Default delimiter is faster than other delimiter.
 		return str
 				.split(/\r?\n/g)
-				.map((row) => row.replace(/^\s*\|/g, ""))
+				.map((row: string) => row.replace(/^\s*\|/g, ""))
 				.join("\n");
 	} else {
-		const regexp = new RegExp(String.raw`^\s*${delimiter}`, "g");
+		const regexp = new RegExp(`^\\s*${delimiter}`, "g");
 		return str
 				.split(/\r?\n/g)
-				.map((row) => row.replace(regexp, ""))
+				.map((row: string) => row.replace(regexp, ""))
 				.join("\n");
 	}
 };
 
 
-const tm = function tm (strings, ...values) {
+const tm = function tm (strings: string[], ...values: string[]): string {
 	if (strings.length !== values.length + 1) {
 		// Error
 		throw new Error("You should call this as Tagged template literals.");
 	}
 	return trimMargin(
 		strings.reduce(
-			(prev, current, index) => prev + values[index - 1] + current));
+			(prev: string, current: string, index: number) => prev + values[index - 1] + current));
 };
 
 
-module.exports = {
+export {
 	trimMargin,
 	tm,
 };
